a909c66b1484d5720b433a2d52151d03a989ff08
Dinh Anh <dinhanhx@gmail.com>
2023-01-30 16:46:30 +0700

    add PhotoStory class

--- vcc/image_text.py
+++ vcc/image_text.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import List
 
 from dataclass_wizard import JSONWizard
 from dataclass_wizard.enums import LetterCase
@@ -17,8 +18,41 @@ class ImageCaption(JSONWizard):
     article_url: str
 
 
+@dataclass
+class ImageDescription:
+    image_url: str
+    description: List[str]
+
+
+@dataclass
+class PhotoStory(JSONWizard):
+    class _(JSONWizard.Meta):
+        # Sets the target key transform to use for serialization;
+        # defaults to `camelCase` if not specified.
+        key_transform_with_load = LetterCase.SNAKE
+        key_transform_with_dump = LetterCase.SNAKE
+
+    contents: List[ImageDescription]
+    article_url: str
+    title: str
+
+    def echo(self):
+        print(self.title)
+        print(self.article_url)
+        for image_description in self.contents:
+            print(image_description.image_url)
+            for description in image_description.description:
+                print(description)
+
+
 if __name__ == '__main__':
-    example = ImageCaption(image_url='http://example.com',
-                           caption='This is a caption',
-                           article_url='http://example.com')
-    print(example.to_dict())
+    article = PhotoStory([
+        ImageDescription(image_url='direct image link', description=[
+            'Describe the image',
+            'Describe the image more'
+        ])
+    ],
+        article_url='article url',
+        title='A title')
+    print(article.to_dict())
+    article.echo()
